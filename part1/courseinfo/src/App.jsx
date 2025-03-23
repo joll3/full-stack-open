0f3752fbf23aebf205c28d6f173a4a8080fe7735
joll3 <5497641+joll3@users.noreply.github.com>
2025-03-23 19:24:19 +0200

    1.2: Course Information, step 2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -15,16 +15,18 @@ const App = () => {
   const Content = (props) => {
     return (
       <div>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
+        <Part name={props.part1} exercises={exercises1} />
+        <Part name={props.part2} exercises={exercises2} />
+        <Part name={props.part3} exercises={exercises3} />
       </div>
+    )
+  }
+
+  const Part = (props) => {
+    return (
+      <p>
+        {props.name} {props.exercises}
+      </p>
     )
   }
 
